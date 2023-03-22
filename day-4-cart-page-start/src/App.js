8c12ef61c9635711e800f6a341511b906c885433
Hesham <heshamelmasry1992@gmail.com>
2023-03-22 11:58:21 +0100

    add day 4 start

---
+++ day-4-cart-page-start/src/App.js
@@ -0,0 +1,15 @@
+import Router from "./routes/Router"
+import Loader from "./components/shared/Loader";
+import {useSelector} from "react-redux";
+
+function App() {
+    const {isLoading} = useSelector(state => state.loader)
+    return (
+        <>
+            <Router/>
+            {isLoading && <Loader/>}
+        </>
+    );
+}
+
+export default App;
---
+++ day-4-cart-page-start/src/routes/Router.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import HomePage from "../components/views/HomePage";
+import ProductDetailsPage from "../components/views/ProductDetailsPage";
+
+function Router() {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/products" element={<HomePage/>}/>
+                <Route path="/product/:id" element={<ProductDetailsPage/>}/>
+            </Routes>
+        </>
+    );
+}
+
+export default Router;
