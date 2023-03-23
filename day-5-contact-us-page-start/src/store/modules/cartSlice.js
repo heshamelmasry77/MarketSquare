c17c4442300841918b576e48f72d56b5d916c4e8
Hesham <heshamelmasry1992@gmail.com>
2023-03-23 15:48:06 +0100

    add day 5

--- day-5-contact-us-page-start/src/store/modules/cartSlice.js
+++ day-5-contact-us-page-start/src/store/modules/cartSlice.js
@@ -9,9 +9,15 @@ const cartSlice = createSlice({
     reducers: { // here we write the functions which will update the state
         ADD_PRODUCT_TO_CART: (state, action) => {
             // TODO i will update my productsInCart
-            console.log("action: ",action);
+            console.log("action: ", action);
             state.productsInCart = [...state.productsInCart, action.payload];
             state.numberOfProductsInCart = state.productsInCart.length
+        },
+        REMOVE_PRODUCT_FROM_CART:(state, action) => {
+            console.log("state:",state);
+            console.log("action:",action);
+            state.productsInCart = state.productsInCart.filter(product => product.id !== action.payload);
+            state.numberOfProductsInCart = state.productsInCart.length;
         }
     }
 })
@@ -20,12 +26,18 @@ export default cartSlice.reducer;
 
 // Actions
 const {ADD_PRODUCT_TO_CART} = cartSlice.actions;
+const {REMOVE_PRODUCT_FROM_CART} = cartSlice.actions
 
 
 export const addSingleProductToCart = (productData) => (dispatch) => {
     console.log("productData: ", productData);
-    dispatch(ADD_PRODUCT_TO_CART(productData))
+    dispatch(ADD_PRODUCT_TO_CART(productData));
     // API CALL TO add product to the user cart in the BE
+}
+
+export const removeSingleProductFromCart = (productId) => (dispatch) => {
+    console.log("productId", productId);
+    dispatch(REMOVE_PRODUCT_FROM_CART(productId));
 }
 
 
--- script.js
+++ script.js
@@ -15,3 +15,37 @@ undefined ? console.log("yes i am happy ") : console.log("no i am not happy")
 
 
 
+
+// filter
+
+const myArray = [
+    { id: 1, name: 'Alice' },
+    { id: 2, name: 'Bob' },
+    { id: 3, name: 'Charlie' }
+];
+
+const idToFilter = 2;
+
+const filteredArray = myArray.filter(name => name.id !== idToFilter);
+
+console.log(filteredArray);
+
+// [ { id: 1, name: 'Alice' }, { id: 3, name: 'Charlie' } ]
+
+
+
+let name = "hesham"
+
+
+function greet(name: string) {
+    console.log(`Hello, ${name}!`);
+}
+
+greet("Alice");
+
+let name: string = "Alice";
+let age: number = 30;
+let isStudent: boolean = true;
+
+console.log(`My name is ${name}, I am ${age} years old, and I am a student: ${isStudent}`);
+
