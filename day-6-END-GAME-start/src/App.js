c3252110962866772f273639ddb328cd4de0e0be
Hesham <heshamelmasry1992@gmail.com>
2023-03-24 03:32:48 +0100

    add day 6 start

---
+++ day-6-END-GAME-start/src/App.js
@@ -0,0 +1,17 @@
+import Router from "./routes/Router"
+import Loader from "./components/shared/Loader";
+import Header from "./components/shared/Header";
+import {useSelector} from "react-redux";
+
+function App() {
+    const {isLoading} = useSelector(state => state.loader)
+    return (
+        <>
+            <Header/>
+            <Router/>
+            {isLoading && <Loader/>}
+        </>
+    );
+}
+
+export default App;
